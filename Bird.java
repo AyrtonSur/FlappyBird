61d2c3cca783ac24f25e134105fc59f39c5c6629
Ayrton Surica <ayrtonsurica@gmail.com>
2024-09-08 21:28:31 -0300

    feat(Bird): começando criação de Bird

---
+++ Bird.java
@@ -0,0 +1,35 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+public class Bird {
+    //Bird
+    private int birdX;
+    private int birdY;
+    private int birdWidth = 34;
+    private int birdHeight = 24;
+    private Image img;
+
+    Bird(Image img, int width, int height) {
+        this.img = img;
+        this.birdWidth = width/8;
+        this.birdHeight = height/2;
+    }
+
+    public int getBirdHeight() {
+        return birdHeight;
+    }
+
+    public int getBirdWidth() {
+        return birdWidth;
+    }
+
+    public Image getImg() {
+        return img;
+    }
+}
