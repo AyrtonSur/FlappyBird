52a657854e8abfcefbfe8ef8bc85da17701d6463
Ayrton Surica <ayrtonsurica@gmail.com>
2024-09-08 22:50:49 -0300

    feat(Pipe): criação e implementação de Pipe

---
+++ Pipe.java
@@ -0,0 +1,69 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+public class Pipe {
+    private int pipeX;
+    private int pipeY;
+    private static int pipeWidth = 64;
+    private static int pipeHeight = 512;
+    private Image img;
+    private boolean passed = false;
+    private int velocityX = 4;
+
+    Pipe(int x, int y, Image img) {
+        this.pipeX = x;
+        this.pipeY = y;
+        this.img = img;
+    }
+
+    public void move(){
+        pipeX -= velocityX;
+    }
+
+    public Image getImg() {
+        return img;
+    }
+
+    public static int getPipeHeight() {
+        return pipeHeight;
+    }
+
+    public static int getPipeWidth() {
+        return pipeWidth;
+    }
+
+    public int getPipeX() {
+        return pipeX;
+    }
+
+    public int getPipeY() {
+        return pipeY;
+    }
+
+    public int getVelocityX() {
+        return velocityX;
+    }
+
+    public void setPassed(boolean passed) {
+        this.passed = passed;
+    }
+
+    public void setVelocityX(int velocityX) {
+        this.velocityX = velocityX;
+    }
+
+    public void setPipeX(int pipeX) {
+        this.pipeX = pipeX;
+    }
+
+    public void setPipeY(int pipeY) {
+        this.pipeY = pipeY;
+    }
+
+}
